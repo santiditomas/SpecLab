7f7a2cbfe04d234ed0269ebcf1e36e9fe5bbad09
Santino Di Tomás <santiditomas@gmail.com>
2025-05-19 16:48:47 -0300

    plot function

--- SpecLab/analysis/calibration.py
+++ SpecLab/analysis/calibration.py
@@ -34,6 +34,7 @@ def calibration_curve(
     ax.plot(X, predictions, label=f"Lineal regression\n$R^2$ = {r2:.3f}", color="red")
     ax.legend()
     ax.grid(True)
+    fig.show()
 
     return fig, {"slope": slope, "intercept": intercept, "r2": r2}       
 
